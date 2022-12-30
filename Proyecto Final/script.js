5979bad7829dcdcbdbe336fa3f1ddc22f54a1eee
zoygio <115259097+zoygio@users.noreply.github.com>
2022-12-30 13:04:40 -0600

    Add files via upload

---
+++ Proyecto Final/script.js
@@ -0,0 +1,10 @@
+const fulImgBox = document.getElementById("fulImgBox")
+fulImgBox = document.getElementById("fulImg");
+
+function openFulImg(reference){
+    fulImgBox.style.display = "flex"
+    fulImg.src = reference
+}
+function closeImg(){
+    fulImgBox.style.display = "none";
+}
